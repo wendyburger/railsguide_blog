3c115ee1a2efd3fb257c209ca9fa4c4a54fe856f
Wendy Yang <wendy.y.w.yang@gmail.com>
2015-11-17 15:30:34 +0800

    Added comment form belong_to article

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :articles do
     resources :comments
-  end
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
